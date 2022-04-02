d455f329a2220bd877352764981a8ffb8729218e
Lucas Chagas Henrichs <84094692+henrichslucas@users.noreply.github.com>
2022-04-02 12:31:24 -0300

    UPDATE 02/04
    ADICIONADO: Menu de dificuldade dinâmico e ajustes no posicionamento dos elementos.

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ let displayType = document.querySelector(".displayType")
 let displayWord = document.querySelector(".displayWord")
 let missCounter = document.querySelector(".missCount")
 let retryBtn = document.querySelector(".retryBtn")
+let easyBtn = document.querySelector(".easyBtn")
+let hardBtn = document.querySelector(".hardBtn")
+let menuContainer = document.querySelector(".diffMenu")
 let i = 0
 let miss = 0
 let timeleft = 10;
 let ready = 0
-let words = [
-    "Pá", "Pé", "Batata", "Sapo", "Casa", "Papel", "Robô", "Laço", "Floresta", "Paralelepipedo", "Hipopótamo", "Desfibrilador", "Eletromagnetismo"
-]
+let words = ["a"]
+let hardWords = ["Floresta", "Paralelepipedo", "Hipopótamo", "Desfibrilador", "Eletromagnetismo"]
+let easyWords = ["Pá", "Pé", "Batata", "Sapo", "Casa", "Papel", "Robô", "Laço"]
 
 let countDown = setInterval(function () {
     if (ready == 1) {
-        
         if (timeleft <= 0) {
             clearInterval(countDown)
             document.querySelector(".countdown").innerHTML = "Tempo esgotado"
@@ -27,10 +29,23 @@ let countDown = setInterval(function () {
     }
 }, 900);
 
-displayWord.innerHTML = words[i]
-displayType.addEventListener("click", function () {
+easyBtn.addEventListener("click", function(){
+    words = [...easyWords]
+    displayWord.innerHTML = words[i]
     ready = 1
+    mainContainer.removeChild(menuContainer)
     displayWord.setAttribute("show", "visible")
+    displayType.setAttribute("show", "visible")
+    displayType.placeholder = " "
+})
+
+hardBtn.addEventListener("click", function(){
+    words = [...hardWords]
+    displayWord.innerHTML = words[i]
+    ready = 1
+    mainContainer.removeChild(menuContainer)
+    displayWord.setAttribute("show", "visible")
+    displayType.setAttribute("show", "visible")
     displayType.placeholder = " "
 })
 
@@ -60,3 +75,21 @@ displayType.addEventListener("keypress", function(event){
 retryBtn.addEventListener("click", function(){
     location.reload(true)
 })
+
+easyBtn.addEventListener("mouseenter", function(){
+    easyBtn.innerHTML = "&#9656;Fácil"
+})
+
+easyBtn.addEventListener("mouseleave", function(){
+    easyBtn.innerHTML = "Fácil"
+})
+
+hardBtn.addEventListener("mouseenter", function(){
+    hardBtn.innerHTML = "&#9656;Difícil"
+})
+
+hardBtn.addEventListener("mouseleave", function(){
+    hardBtn.innerHTML = "Difícil"
+})
+
+
